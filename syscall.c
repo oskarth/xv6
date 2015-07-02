a72ea5611a2ff8f730bbf96ab6fe9dff5e4cc942
oskarth <me@oskarth.com>
2015-07-02 08:19:29 -0400

    add syscall_names array and print

--- syscall.c
+++ syscall.c
@@ -123,6 +123,30 @@ static int (*syscalls[])(void) = {
 [SYS_close]   sys_close,
 };
 
+static char (*syscall_names[]) = {
+[SYS_fork]    "fork",
+[SYS_exit]    "exit",
+[SYS_wait]    "wait",
+[SYS_pipe]    "pipe",
+[SYS_read]    "read",
+[SYS_kill]    "kill",
+[SYS_exec]    "exec",
+[SYS_fstat]   "fstat",
+[SYS_chdir]   "chdir",
+[SYS_dup]     "dup",
+[SYS_getpid]  "getpid",
+[SYS_sbrk]    "sbrk",
+[SYS_sleep]   "sleep",
+[SYS_uptime]  "uptime",
+[SYS_open]    "open",
+[SYS_write]   "write",
+[SYS_mknod]   "mknode",
+[SYS_unlink]  "unlink",
+[SYS_link]    "link",
+[SYS_mkdir]   "mkdir",
+[SYS_close]   "close"
+};
+
 void
 syscall(void)
 {
@@ -131,6 +155,8 @@ syscall(void)
   num = proc->tf->eax;
   if(num > 0 && num < NELEM(syscalls) && syscalls[num]) {
     proc->tf->eax = syscalls[num]();
+    cprintf("%s -> %d\n",
+            syscall_names[num], proc->tf->eax);
   } else {
     cprintf("%d %s: unknown sys call %d\n",
             proc->pid, proc->name, num);
