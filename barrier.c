07c1f11a8f9367db4e259578e4e6e96b243e2648
oskarth <me@oskarth.com>
2015-08-07 00:26:36 +0200

    barriers

--- barrier.c
+++ barrier.c
@@ -27,7 +27,17 @@ barrier_init(void)
 static void 
 barrier()
 {
-  bstate.round++;
+  pthread_mutex_lock(&bstate.barrier_mutex);
+  bstate.nthread++;
+  if (bstate.nthread == nthread) {
+    pthread_cond_broadcast(&bstate.barrier_cond);
+    bstate.nthread = 0;
+    bstate.round++;
+  }
+  else {
+    pthread_cond_wait(&bstate.barrier_cond, &bstate.barrier_mutex);
+  }
+  pthread_mutex_unlock(&bstate.barrier_mutex);
 }
 
 static void *
